44dee86451c7742bbc2190f265f97f5947bfa8a6
Isaac Chua <isaac@Isaacs-MacBook-Air-2.local>
2025-10-09 12:50:07 +1030

    GameCharacter class attributes and methods initialised

---
+++ GameCharacter.h
@@ -0,0 +1,27 @@
+#ifndef GAMECHARACTER_H
+#define GAMECHARACTER_H
+
+struct Coordinate{
+    public:
+    int row;
+    int col;    
+    int getRow(GameCharacter* gc);
+    int getCol();
+};
+
+class GameCharacter{
+    protected:
+        Coordinate position;
+        int health;
+    
+    public:
+        GameCharacter();
+        GameCharacter(int hp);
+        virtual ~GameCharacter();
+        int getHealth(GameCharacter* gc);
+        virtual void takeDamage(int dmg);
+        virtual void update() = 0;
+};
+
+
+#endif
